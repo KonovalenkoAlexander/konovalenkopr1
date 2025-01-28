9a159f0b91fed299ed8376e57e6ee40df307b023
KonovalenkoAlexander <166617042+KonovalenkoAlexander@users.noreply.github.com>
2025-01-28 16:15:20 +0200

    Delete js directory

--- js/app/widgets/footer.js
+++
@@ -1,7 +0,0 @@
-export default {
-  template: `
-    <footer style="background-color: #333; color: white; padding: 10px; text-align: center; margin-top: auto;">
-      <p>&copy; 2025 My Vue App. All rights reserved.</p>
-    </footer>
-  `,
-};
